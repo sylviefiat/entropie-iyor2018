7863a7aad13afb89f97d71e30a4ba55c8835d4f2
sylviefiat <sylvie.fiat@ird.fr>
2018-03-07 16:39:38 +1100

    update

--- pres.js
+++ pres.js
@@ -3,23 +3,68 @@
  */
 
 var slideCount = 0;
+var slideProject = 4;
 var slideArray = [];
 var currentSlide = 1;
+var videoCount = 0;
+var videoArray = [];
+var currentVideo = 0;
+var direction = 1;
+var $sl, interval;
 
-var players = [];
-var playingID = null;
+
+function slidit(container, delta){	
+  	var max = -(container.width() - $(window).width());
+	// new position = old position + deltaY
+	var posX = container.offset().left -= delta;
+	    // boundaries calculation
+	    if(posX >= 0) { posX = max;}
+	    if(posX <= max) { posX = 0;}
+	    // update position
+	    container.offset({left: posX});  	
+};
+
+function startSlider(){
+	interval = setInterval(function()
+    	{
+        	slidit($('.images'), $(window).width()/2);
+    	}, 5000);
+}
+
+function stopSlider(){
+	console.log("stop");
+	clearInterval(interval);
+}
 
 function positionSlide(slide) {
+	stopSlider();
+	// display background video on first slide only
+	$("#bgvid").css("display", (slide>1)?"none":"block");
+
 	if (slide > 0 && slide <= slideCount) {
+		// remove previous slide
 		slideArray[currentSlide].fadeOut(500);
+		if(slideArray[currentSlide].find("iframe") && slideArray[currentSlide].find("iframe")[0]){
+			slideArray[currentSlide].find("iframe")[0].src="";
+		}
+		// new slide
 		currentSlide = slide;
 		slideArray[currentSlide].fadeIn(1500, 'swing');
-		// display background video on first slide only
-		$("#bgvid").css("display", (slide>1)?"none":"block");
-		// start video player on current slide
-		if(slideArray[currentSlide].find("iframe")!==null && slideArray[currentSlide].find("iframe").length > 0){
-			
+		if(slideArray[currentSlide].find("iframe") && slideArray[currentSlide].find("iframe")[0]){
+			currentVideo++;
+			slideArray[currentSlide].find("iframe")[0].src = videoArray[currentVideo];	
+		} else {
+			currentVideo=0;
 		}
+	}
+
+	if(slide === 3){
+		startSlider();
+    	$('.images').hover(function(){
+		    stopSlider();
+		    }, function(){
+		    startSlider();
+		});
 	}
 
 }
@@ -60,7 +105,12 @@ $(function() {
 	});
 	$(document).keyup(handleKeyup);
 	slideArray[currentSlide].css("display", "flex");
-
+	// video url in array
+	$(".video").each(function () {
+		videoCount++;
+		videoArray[videoCount]=$(this).find("iframe")[0].src;
+		$(this).find("iframe")[0].src = null;
+	});
 });
 
 document.addEventListener("wheel", function (e) {
@@ -71,14 +121,9 @@ document.addEventListener("wheel", function (e) {
 		else
 			nextSlide();
 	} else {
-		var $div = $('.images'),
-      	max = -($div.width() - $(window).width());
-		// new position = old position + deltaY
-	    var posX = $div.offset().left -= e.deltaY;
-	    // boundaries calculation
-	    if(posX >= 0) { posX = 0; }
-	    if(posX <= max) { posX = max; }
-	    // update position
-	    $div.offset({left: posX});
+		slidit($('.images'), e.deltaY);
 	}
 }, true);
+
+
+
